90f26ec73b12e704b6625ca8139ccf04c0648ce8
AnShengLi88 <15534137876@163.com>
2019-11-20 18:11:28 +0800

    1

---
+++ Unity_LearnGitHub/Assets/Scripts/Test_Camera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_Camera : MonoBehaviour {
+
+
+	void Start ()
+    {
+        Debug.Log("hahahahahah");
+	}
+	
+	
+	void Update ()
+    {
+		
+	}
+}
