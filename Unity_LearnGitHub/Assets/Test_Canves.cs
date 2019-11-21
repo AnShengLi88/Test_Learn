5d1d1e9364d93ac490e1db799ef5ca9556bbec02
AnShengLi88 <15534137876@163.com>
2019-11-21 09:46:39 +0800

    新加一个界面

---
+++ Unity_LearnGitHub/Assets/Test_Canves.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Test_Canves : MonoBehaviour
+{
+
+    public Image Image_bg;
+    public GameObject Button_Close;
+    public ScrollRect ScrollRect_None;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
